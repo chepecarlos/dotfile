b8e446d237d67a98d897894d3608db274f5936c4
chepecarlos <chepecarlos@alswblog.org>
2021-06-06 16:01:33 -0600

    Agregengando menu y opciones a plugin de Blender

--- .config/blender/2.92/scripts/addons/Plugin_ALSW.py
+++ .config/blender/2.92/scripts/addons/Plugin_ALSW.py
@@ -1,5 +1,12 @@
 import bpy
 
+from bpy.props import (
+        BoolProperty,
+        FloatProperty,
+        EnumProperty,
+        IntProperty,
+        )
+
 bl_info = {
     "name": "Heramientas ALSW",
     "author": "Jose Carlos (ALSW)",
@@ -10,24 +17,56 @@ bl_info = {
     "category": "Object",
 }
 
-def MostarMensajeBot(message="", title="Message Box", icon='INFO'):
+def MostarMensajeBox(message="", title="Message Box", icon='INFO'):
     def draw(self, context):
         self.layout.label(text=message)
     bpy.context.window_manager.popup_menu(draw, title=title, icon=icon)
 
 
-class insertVideo(bpy.types.Operator):
-    bl_idname = "scene.invideo"
+class audioespecial(bpy.types.Operator):
+    bl_idname = "scene.audioespecial"
     bl_label = "Insert Video"
+    bl_description = "Insertar pista de audio sobre otra clip"
+    bl_options = {"REGISTER", "UNDO"}
+
+    # pista : EnumProperty(
+    #         items=(
+    #             (0, 'alsw', "ALSW"),
+    #             (1, 'mrtee', "MrTee"),
+    #             ),
+    #         name="Pista",
+    #         default= 0,
+    #         description="Pista de audio"
+    #         )
+
+    pollo : FloatProperty(
+            name="pollo",
+            description="zoon para clip",
+            default=1,
+            min=-10, max=10
+            )
+
+    pista: EnumProperty(
+        name="Type of radius",
+        description="Choose type of atom radius",
+        items=(('alsw', "ALSW", "Use pre-defined radius"),
+               ('mrtee', "MrTee", "Use atomic radius"),
+               ('p', "van der Waals", "Use van der Waals radius")),
+        default='mrtee',)
+
+    # Verifica que este alguna secuencia selecionada
+    @classmethod
+    def poll(cls, context):
+        return context.selected_sequences
 
     def execute(self, context):
-
+        print("Pista ", self.pista)
         VideoActual = "/home/chepecarlos/2.VideoMusicales/demo/POP.mkv"
 
         # context.area.type = 'SEQUENCE_EDITOR'
         # FrameActual = bpy.context.scene.frame_current
 
-        if len(bpy.context.selected_sequences) > 0:
+        if len(context.selected_sequences) > 0:
             Inicio = context.selected_sequences[0].frame_final_start
             Final = context.selected_sequences[0].frame_final_end
             Canal = context.selected_sequences[0].channel + 1
@@ -42,26 +81,39 @@ class insertVideo(bpy.types.Operator):
 
             # bpy.context.selected_sequences[0].use_proxy = True
         else:
-            MostarMensajeBot("Selecione una pista", title="Error", icon="ERROR")
+            MostarMensajeBox("Selecione una pista", title="Error", icon="ERROR")
         return{'FINISHED'}
 
 class superzoon(bpy.types.Operator):
     bl_idname = "scene.superzoon"
     bl_label = "Super Zoon"
-    PosicionY = 0
-    PosicionX = 0
+    bl_description = "Cambia el zoon en clip"
+    bl_options = {"REGISTER", "UNDO"}
+
+    zoon : FloatProperty(
+            name="zoon",
+            description="zoon para clip",
+            default=1,
+            min=-10, max=10
+            )
+
+    # Verifica que este alguna secuencia selecionada
+    @classmethod
+    def poll(cls, context):
+        return context.selected_sequences
 
     def execute(self, context):
 
         if len(bpy.context.selected_sequences) > 0:
-            print(PosicionY)
-            ClipActual = bpy.context.selected_sequences[0]
-            PosicionX = ClipActual.transform.offset_x
-            PosicionY = ClipActual.transform.offset_y
-            EscalaX = ClipActual.transform.scale_x
-            EscalaY = ClipActual.transform.scale_y
-            ClipActual.transform.scale_x = EscalaX*2
-            ClipActual.transform.scale_y = EscalaY*2
+            print("zoon ",self.zoon)
+
+            # ClipActual = bpy.context.selected_sequences[0]
+            # PosicionX = ClipActual.transform.offset_x
+            # PosicionY = ClipActual.transform.offset_y
+            # EscalaX = ClipActual.transform.scale_x
+            # EscalaY = ClipActual.transform.scale_y
+            # ClipActual.transform.scale_x = EscalaX*2
+            # ClipActual.transform.scale_y = EscalaY*2
         else:
             ShowMessageBox("Selecione una pista", title="Error", icon="ERROR")
 
@@ -77,16 +129,45 @@ class MyPanel(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        # obj = context.object
 
         row = layout.row()
-        row.label(text="Musica Velocidad")
+        row.label(text="Musica sobre clip")
         row = layout.row()
-        row.operator("scene.invideo", text="Agregar Sonido MrTee")
+        ops = row.operator("scene.audioespecial", text="MrTee")
+        ops.pista = "mrtee"
+        ops = row.operator("scene.audioespecial", text="ALSW")
+        ops.pista = "alsw"
+
+        layout.separator()
         row = layout.row()
-        ops = row.operator("scene.superzoon", text="Has Zoon")
+        row.label(text="Alineacion")
+        row = layout.row()
+        row.operator("scene.audioespecial", text="Izquierda")
+        row.operator("scene.audioespecial", text="Centro")
+        row.operator("scene.audioespecial", text="Derecha")
+        row = layout.row()
+        row.operator("scene.audioespecial", text="Abajo")
+        row.operator("scene.audioespecial", text="Centro")
+        row.operator("scene.audioespecial", text="Ariba")
+
+        row = layout.row()
+        row.label(text="Zoon", icon="ZOOM_IN")
+        row = layout.row()
+        ops = row.operator("scene.superzoon", text="0.5X")
+        ops.zoon = 0.5
+        ops = row.operator("scene.superzoon", text="1X")
+        ops.zoon = 1
+        ops = row.operator("scene.superzoon", text="2X")
+        ops.zoon = 2
 
 addon_keymaps = []
+
+# Registar clases
+classes = [
+    MyPanel,
+    audioespecial,
+    superzoon
+]
 
 def add_hotkey():
     print("Agregando Teclas Rapida")
@@ -95,7 +176,11 @@ def add_hotkey():
 
     km = kc.keymaps.new(name='Sequencer', space_type='SEQUENCE_EDITOR')
 
-    kmi = km.keymap_items.new("scene.invideo", 'M', 'PRESS', ctrl=True, shift=False)
+    kmi = km.keymap_items.new("scene.audioespecial", 'M', 'PRESS', ctrl=True, shift=False)
+
+    kmi = km.keymap_items.new("scene.superzoon", type="P", value="PRESS", ctrl=True, shift=False)
+    kmi.properties.zoon = 1
+
     addon_keymaps.append((km, kmi))
 
 def remove_hotkey():
@@ -103,20 +188,19 @@ def remove_hotkey():
         km.keymap_items.remove(kmi)
     addon_keymaps.clear()
 
-
 def register():
-    bpy.utils.register_class(MyPanel)
-    bpy.utils.register_class(insertVideo)
-    bpy.utils.register_class(superzoon)
+    from bpy.utils import register_class
+
+    for cls in classes:
+        register_class(cls)
     add_hotkey()
 
 
 def unregister():
     remove_hotkey()
-    bpy.utils.unregister_class(MyPanel)
-    bpy.utils.unregister_class(insertVideo)
-    bpy.utils.unregister_class(superzoon)
-
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
 
 if __name__ == "__main__":
     register()
