c69954e1d8637b582856d1abbe18e6c21af24189
chepecarlos <chepecarlos@alswblog.org>
2023-10-30 19:35:21 -0600

    agregando codigo de instalacion de paquetes y reparar wifi de usb

---
+++ .config/instalarChepeCarlos/instalar.sh
@@ -0,0 +1,7 @@
+
+sudo apt update
+sudo apt upgrade
+
+sudo apt install -y git mint-meta-codecs dkms vlc filezilla audacity bleachbit cheese shutter meld okular openssh-server ffmpeg pavucontrol nmap gitg scrcpy barrier exa gromit-mpx
+
+flatpak install  --assumeyes -y -v com.visualstudio.code org.inkscape.inkscape org.gimp.GIMP com.obsproject.Studio org.telegram.desktop com.discordapp.Discord org.blender.blender com.github.tchx84.Flatseal io.github.mimbrero.WhatsAppDesktop com.prusa3d.PrusaSlicer io.missioncenter.MissionCenter org.freecadweb.FreeCAD 
---
+++ .config/instalarChepeCarlos/wifi.sh
@@ -0,0 +1,4 @@
+sudo apt install git dkms
+git clone https://github.com/cilynx/rtl88x2bu.git
+sudo dkms add ./rtl88x2bu
+sudo dkms install rtl88x2bu/5.8.7.1
