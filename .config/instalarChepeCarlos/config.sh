bcb319d754c79207b32c849cbd1e8da8c7184139
chepecarlos <chepecarlos@alswblog.org>
2023-11-04 09:46:23 -0600

    agregando cambiador de esquinas

--- .config/instalarChepeCarlos/config.sh
+++ .config/instalarChepeCarlos/config.sh
@@ -1,2 +1,10 @@
+
+# Investigar con esto
+# gsettings list-recursively >> polloBase.txt
+
+
 # quitar iconos de escritorio
 gsettings set org.nemo.desktop desktop-layout 'false::false'
+
+# cambia escritorio y ventanas en esquinas
+gsettings set org.cinnamon hotcorner-layout "['expo:true:0', 'expo:true:0', 'scale:true:0', 'scale:true:0']"
