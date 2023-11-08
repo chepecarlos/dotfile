d29428eda6172938e54450b9aa94a28ff05596f0
chepecarlos <chepecarlos@alswblog.org>
2023-11-07 18:06:15 -0600

    boton de zoon y boton de menu

--- .config/instalarChepeCarlos/config.sh
+++ .config/instalarChepeCarlos/config.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 
 # Investigar con esto
 # gsettings list-recursively >> polloBase.txt
@@ -7,3 +8,11 @@ gsettings set org.nemo.desktop desktop-layout 'false::false'
 
 # cambia escritorio y ventanas en esquinas
 gsettings set org.cinnamon hotcorner-layout "['expo:true:0', 'expo:true:0', 'scale:true:0', 'scale:true:0']"
+
+# activar zoon en ventana
+gsettings set org.cinnamon.desktop.a11y.applications screen-magnifier-enabled true
+gsettings set org.cinnamon.desktop.wm.preferences mouse-button-zoom-modifier '<Super>'
+
+#Repar Problema con Blender con Agarar bentanss Alt
+# Super para arrastrar ventanas
+gsettings set org.cinnamon.desktop.wm.preferences mouse-button-modifier '<Super>'
