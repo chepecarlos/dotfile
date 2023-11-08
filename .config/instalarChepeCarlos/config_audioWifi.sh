e15954e175480272b1f2c3e952a31d6a8b6ad69a
chepecarlos <chepecarlos@alswblog.org>
2023-11-07 18:04:41 -0600

    agregando como corregir audio wifi

---
+++ .config/instalarChepeCarlos/config_audioWifi.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Carpeta config pulse audio
+ls /usr/lib | grep pulse
+
+# Version: pulse-13.99.1
+# sudo ln -s /usr/lib/pulse-13.99.1/ /usr/lib/pulse-13.99
+
+# Version: pulse-15.99.1+dfsg1
+sudo ln -s /usr/lib/pulse-15.99.1+dfsg1/ /usr/lib/pulse-15.99
